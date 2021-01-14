3409ad04baec5aaa3e945227dd89de5e2e2f92d7
pouyapanahandeh <panahandehpooya@gmail.com>
2021-01-14 20:57:24 +0100

    peer js change to not secure to check if it's work

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 document.getElementById('joinLink').value = window.location.href
 const myPeer = new Peer({
-  secure: true,
+  secure: false,
   host: 'moon-call.herokuapp.com',
   port: 9000
 })

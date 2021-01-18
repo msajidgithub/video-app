9da194e8bcdbbf70c5166836f11d84c08aa1e423
pouyapanahandeh <panahandehpooya@gmail.com>
2021-01-18 22:08:57 +0100

    delete the port number

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ document.getElementById('joinLink').value = window.location.href
 const myPeer = new Peer(undefined, {
     secure: true,
     host: 'moon-call.herokuapp.com',
-    port: '3001'
+    // port: '3001'
   })
 const myVideo = document.createElement('video')
 myVideo.muted = true

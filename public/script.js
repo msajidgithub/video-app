2650f017494a25a854e01f4b8c787249ba8ef678
Muhammad Sajid <mxsajidxr@gmail.com>
2021-06-25 13:02:54 +0500

    key changed

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 // document.getElementById('joinLink').value = window.location.href
 const myPeer = new Peer({
-  key: 'peerjs',
+  key: 'dopplr',
   host: 'new-peer.herokuapp.com',
   port: '9000',
   path: '/wpc-video',
